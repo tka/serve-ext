0138bc8657b8affef687465d785f9b017351ea5c
tka.lu <tkatka@gmail.com>
2015-11-21 00:34:15 +0800

    fix capture_slim

--- lib/serve_ext/view_helpers.rb
+++ lib/serve_ext/view_helpers.rb
@@ -5,13 +5,7 @@
 module Serve #:nodoc:
   module ContentHelpers
     def capture_slim(&block)
-      old_buffer = eval  "_buf", block.binding
-      eval  "origin_buf,_buf=_buf,[]", block.binding
-      capture_content = yield.to_s
-      eval  "_buf=origin_buf", block.binding
-      capture_content
-    ensure
-      eval  "_buf=origin_buf", block.binding
+      yield
     end
   end
 
